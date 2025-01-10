c19e9f617a629f6529c35ae578a82ebb4a62d826
Daksh Purohit <dakshgrows@gmail.com>
2025-01-10 18:13:54 +0530

    done with arrays

---
+++ 04_JS Foundation/part2/nonPrimitives.js
@@ -0,0 +1,41 @@
+const username = {
+    "first name":"daksh",
+    isLoggedIn: true
+}
+
+username.firstname = "Mr. D"
+username.lastname = "purohit"
+
+// console.log(username);
+// console.log(username["firstname"])
+// console.log(username["lastname"])
+// console.log(typeof username)
+
+// let today = new Date();
+// console.log(today)
+// console.log(today.getDate())
+// console.log(today.getTime())
+
+//Array
+let anotherUser = [false, "purohit", "daksh", true]
+// console.log(anotherUser[0])
+
+// console.log("1" + 1)
+// console.log(1 + "1")
+
+// let isValue = true;
+// let otherValue = false;
+// console.log(Number(isValue))
+// console.log(Number(otherValue))
+// console.log(isValue + 1);
+// console.log(otherValue + 1);
+
+
+let isValue = "2abac";
+console.log(isValue)
+console.log(Number(isValue)) //NaN
+console.log(typeof Number(isValue))
+console.log(typeof null); //object
+console.log(Number(null)); //0
+console.log(typeof Number(null)); //number
+console.log(Number(undefined)); //NaN
---
+++ 04_JS Foundation/part3/conditional-challenges.js
@@ -0,0 +1,55 @@
+//Checking if a number is greater than another number
+let num1 = 5;
+let num2 = 6;
+/*
+console.log("I am regular upper code");
+
+if(num1 > num2) {
+    console.log("num1 is greater than num2")
+} else {
+    console.log("num2 is greater than num1")
+}
+
+console.log("I am regular bottom code");
+*/
+
+
+//Checking if a string is equal to another string
+/*
+let username = "chai"
+let anotherusername = "chai"
+if(username == anotherusername) {
+    console.log("Pick another username")
+} else {
+    console.log("You can pick this username");
+}
+*/
+
+//Checking if a variable is number or not
+/*
+let score = "44";
+if(typeof score === "number") {
+    console.log("Yep, this is a number");
+} else {
+    console.log("No, that is not a number");
+}
+*/
+
+//Checking if a boolean value is true or false
+/*
+let isTeaReddy = false;
+if(isTeaReddy) {
+    console.log("Tea is ready")
+} else {
+    console.log("Tea is not ready")
+}
+*/
+
+//Checking if an array is empty or not
+let items = ["item 1"]
+console.log(items.length)
+if (items.length === 0) {
+    console.log("Array is empty")
+} else {
+    console.log("Array is not empty")
+}
